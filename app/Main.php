5ab95b1333bf189d8c1fce17ea28c35756881e99
Benjy <invartam@gmail.com>
2017-02-25 01:52:39 +0100

    Update Main

--- app/Main.php
+++ app/Main.php
@@ -2,17 +2,7 @@
 namespace App;
 
 use Exception;
-use App\Model\CD37;
-use App\Model\School;
 use App\Loader\ExcelLoader;
-use Fortinet\Fortigate\Fortigate;
-use Fortinet\Fortigate\Address;
-use Fortinet\Fortigate\AddressGroup;
-use Fortinet\Fortigate\Service;
-use Fortinet\Fortigate\ServiceGroup;
-use Fortinet\Fortigate\NetDevice;
-use Fortinet\Fortigate\Zone;
-use Fortinet\Policy\Policy;
 
 class Main {
 
