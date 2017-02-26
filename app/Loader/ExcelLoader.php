bcc5d4996383ca4b3478385d48ce5b9dea0770a1
Benjy <invartam@gmail.com>
2017-02-26 14:50:20 +0100

    Add serviceGroup support

--- app/Loader/ExcelLoader.php
+++ app/Loader/ExcelLoader.php
@@ -2,9 +2,12 @@
 namespace App\Loader;
 
 use Exception;
+use Fortinet\Fortigate\Fortigate;
+use Fortinet\Fortigate\NetDevice;
 use Fortinet\Fortigate\Address;
 use Fortinet\Fortigate\AddressGroup;
-use Fortinet\Fortigate\NetDevice;
+use Fortinet\Fortigate\Service;
+use Fortinet\Fortigate\ServiceGroup;
 use Fortinet\Fortigate\Zone;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
@@ -20,10 +23,6 @@ class ExcelLoader {
 
   private $source;
   private $fortigate;
-  private $addresses = [];
-  private $addressGroups = [];
-  private $interfaces = [];
-  private $zones = [];
 
   private function getInfos()
   {
@@ -55,8 +54,8 @@ class ExcelLoader {
           throw new Exception("Interface $name is of type VLAN, there must be one and only one physical interface set", 1);
         }
         foreach ($aDevices as $device) {
-          if (!array_key_exists($device, $this->interfaces)) {
-            $this->interfaces[$device] = new NetDevice($device);
+          if (!array_key_exists($device, $this->fortigate->interfaces)) {
+            $this->fortigate->addNetDevice(new NetDevice($device));
           }
         }
       }
@@ -65,23 +64,24 @@ class ExcelLoader {
       $alias = trim($row->getCellIterator()->seek("H")->current()->getValue());
       $vdom = trim($row->getCellIterator()->seek("I")->current()->getValue());
       $zone = trim($row->getCellIterator()->seek("J")->current()->getValue());
-      if (!empty($zone) && !array_key_exists($zone, $this->zones)) {
-        $this->zones[$zone] = new Zone();
-        $this->zones[$zone]->setName($zone);
+      if (!empty($zone) && !array_key_exists($zone, $this->fortigate->zones)) {
+        $newZone = new Zone();
+        $newZone->setName($zone);
+        $this->fortigate->addZone($newZone);
       }
-      if (array_key_exists($name, $this->interfaces)) {
+      if (array_key_exists($name, $this->fortigate->interfaces)) {
         throw new Exception("Duplicate interface $name at row " . $row->getRowIndex(), 1);
       }
       $if = new NetDevice($name);
       if ($type == "VLAN") {
         $if->setType(NetDevice::VLAN);
         $if->setVlanID($vlanid);
-        $if->setVlanDevice($this->interfaces[$devices]);
+        $if->setVlanDevice($this->fortigate->interfaces[$devices]);
       }
       if ($type == "LAGG") {
         $if->setType(NetDevice::LAGG);
         foreach ($aDevices as $device) {
-          $if->addLaggNetDevice($this->interfaces[$device]);
+          $if->addLaggNetDevice($this->fortigate->interfaces[$device]);
         }
       }
       if (!empty($ip)) {
@@ -94,8 +94,10 @@ class ExcelLoader {
       if (!empty($alias)) {
         $if->setAlias($alias);
       }
-      $this->zones[$zone]->addInterface($if);
-      $this->interfaces[$if->getName()] = $if;
+      if (!empty($zone)) {
+        $this->fortigate->zones[$zone]->addInterface($if);
+      }
+      $this->fortigate->addNetDevice($if);
     }
   }
 
@@ -109,10 +111,10 @@ class ExcelLoader {
       if (empty($name) || empty($ip) || empty($mask)) {
         break;
       }
-      if (array_key_exists($name, $this->addresses)) {
+      if (array_key_exists($name, $this->fortigate->addresses)) {
         throw new Exception("Duplicate address $name at row " . $row->getRowIndex(), 1);
       }
-      $this->addresses[] = new Address($name, $ip, $mask);
+      $this->fortigate->addAddress(new Address($name, $ip, $mask));
     }
   }
 
@@ -125,14 +127,79 @@ class ExcelLoader {
       if (empty($name) || empty($member)) {
         break;
       }
-      if (!array_key_exists($member, $this->addresses)) {
-        throw new Exception("Cannot add non existent address $member in address group $name at row " . $row->getRowIndex(), 1);
+      if (!array_key_exists($member, $this->fortigate->addresses) && !array_key_exists($member, $this->fortigate->addressGroups)) {
+        throw new Exception("Cannot add non existent address or address group $member in address group $name at row " . $row->getRowIndex(), 1);
       }
-      if (!array_key_exists($name, $this->addressGroups)) {
-        $this->addressGroups[$name] = new AddressGroup($name);
+      if (!array_key_exists($name, $this->fortigate->addressGroups)) {
+        $this->fortigate->addAddressGroup(new AddressGroup($name));
       }
-      $this->addressGroups[$name]->addAddress($this->addresses[$member]);
+      $this->fortigate->addressGroups[$name]->addAddress($this->fortigate->addresses[$member]);
     }
+  }
+
+  private function parseService()
+  {
+    $sheet = $this->source->getSheetByName(self::TAB_SERVICE);
+    foreach ($sheet->getRowIterator(3) as $row) {
+      $name = trim($row->getCellIterator()->seek("B")->current()->getValue());
+      $portl = trim($row->getCellIterator()->seek("C")->current()->getValue());
+      $porth = trim($row->getCellIterator()->seek("D")->current()->getValue());
+      $proto = trim($row->getCellIterator()->seek("E")->current()->getValue());
+      if (empty($name) || empty($proto)) {
+        break;
+      }
+      $l4proto = "";
+      $l3proto = Service::PROTO_IP;;
+      if ($proto == "UDP") {
+        $l4proto = Service::L4_UDP;
+      }
+      if ($proto == "TCP") {
+        $l4proto = Service::L4_TCP;
+      }
+      if ($proto == "ICMP") {
+        $l3proto = Service::PROTO_ICMP;
+      }
+      if ($l3proto != Service::PROTO_ICMP && (empty($l3proto) || empty($l4proto))) {
+        throw new Exception("Non existent protocol $proto at row " . $row->getRowIndex(), 1);
+      }
+      if (array_key_exists($name, $this->fortigate->services)) {
+        throw new Exception("Duplicate service $name at row " . $row->getRowIndex(), 1);
+      }
+      $portrange = "";
+      if ($l3proto != Service::PROTO_ICMP) {
+        $portrange = empty($porth) ? $portl : "$portl-$porth";
+      }
+      $this->fortigate->addService(new Service($name, $l3proto, $l4proto, $portrange));
+    }
+  }
+
+  private function parseServiceGroup()
+  {
+    $sheet = $this->source->getSheetByName(self::TAB_SERVICEGROUP);
+    foreach ($sheet->getRowIterator(3) as $row) {
+      $name = trim($row->getCellIterator()->seek("B")->current()->getValue());
+      $member = trim($row->getCellIterator()->seek("C")->current()->getValue());
+      if (empty($name) || empty($member)) {
+        break;
+      }
+      if (!array_key_exists($member, $this->fortigate->services) && !array_key_exists($member, $this->fortigate->serviceGroups)) {
+        throw new Exception("Cannot add non existent service or service group $member in service group $name at row " . $row->getRowIndex(), 1);
+      }
+      if (!array_key_exists($name, $this->fortigate->serviceGroups)) {
+        $this->fortigate->addServiceGroup(new ServiceGroup($name));
+      }
+      if (array_key_exists($member, $this->fortigate->services)) {
+        $this->fortigate->serviceGroups[$name]->addService($this->fortigate->services[$member]);
+      }
+      if (array_key_exists($member, $this->fortigate->serviceGroups)) {
+        $this->fortigate->serviceGroups[$name]->addService($this->fortigate->serviceGroups[$member]);
+      }
+    }
+  }
+
+  private function parsePolicy()
+  {
+
   }
 
   private function getFortigate()
@@ -146,13 +213,15 @@ class ExcelLoader {
       throw new Exception("The file $file does not exist", 1);
     }
     $this->source = IOFactory::load($file);
+    $this->fortigate = new Fortigate();
 
     $this->getInfos();
     $this->parseInterfaces();
     $this->parseAddress();
     $this->parseAddressGroup();
-    // $this->parseService();
-    // $this->parseServiceGroup();
+    $this->parseService();
+    $this->parseServiceGroup();
     // $this->parsePolicy();
+    print $this->fortigate;
   }
 }
