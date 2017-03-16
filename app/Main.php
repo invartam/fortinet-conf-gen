e066831531747c1dac40550bc0e803e2559506e2
Benjy <invartam@gmail.com>
2017-03-16 21:47:44 +0100

    Remove unused variables

--- app/Main.php
+++ app/Main.php
@@ -12,8 +12,6 @@ use phpseclib\Net\SSH2;
 
 class Main {
 
-  private static $vrf = [];
-
   private static function unaccent($string)
   {
     return preg_replace('~&([a-z]{1,2})(?:acute|cedil|circ|grave|lig|orn|ring|slash|th|tilde|uml|caron);~i', '$1', htmlentities($string, ENT_QUOTES, 'UTF-8'));
